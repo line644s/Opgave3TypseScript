a849f19ac4ae43889f788b021fa683731c47264f
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-10-02 10:10:47 +0200

    Opgave 3

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,22 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let input: HTMLInputElement = <HTMLInputElement>document.getElementById("input");
+let valg: HTMLSelectElement = <HTMLSelectElement>document.getElementById("valg");
+let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("knap")
+let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+button.addEventListener("click", manipulate)
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function manipulate(): void {
+    if (valg.value === "UpperCase") {
+        element.innerHTML = input.value.toUpperCase();
+    }
+    if (valg.value === "LowerCase") {
+        element.innerHTML = input.value.toLowerCase();
+    }
+    if (valg.value === "DoNothing") {
+        element.innerHTML = input.value;
+    }
+}
+document.addEventListener("keypress", (e: KeyboardEvent) => {if (e.keyCode === 13) {
+    manipulate();
+}
+ } );
